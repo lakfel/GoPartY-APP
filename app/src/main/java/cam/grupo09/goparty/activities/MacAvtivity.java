a9000c49e8ca372615d4cff9202c78b3604a4aa3
Johann Felipe González Ávila <jf.gonzalez695@gmail.com>
2016-05-16 07:41:50 -0500

    22

--- app/src/main/java/cam/grupo09/goparty/activities/MacAvtivity.java
+++ app/src/main/java/cam/grupo09/goparty/activities/MacAvtivity.java
@@ -7,25 +7,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import cam.grupo09.goparty.R;
 
 public class MacAvtivity extends AppCompatActivity {
 
+    private GoogleMap googleMap;
+    static final LatLng TutorialsPoint = new LatLng(21 , 57);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mac_avtivity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+        setContentView(R.layout.activity_main);
+
+        try {
+            if (googleMap == null) {
+                googleMap = ((MapFragment) getFragmentManager().
+                        findFragmentById(R.id.map)).getMap();
             }
-        });
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Marker TP = googleMap.addMarker(new MarkerOptions().
+                    position(TutorialsPoint).title("TutorialsPoint"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
 
 }
